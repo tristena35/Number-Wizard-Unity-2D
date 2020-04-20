3063e65c4c71f573dea1e0948e4daf57f079682a
Tristen Aguilar <tristenaguilar@Tristens-MBP.fios-router.home>
2020-04-20 18:15:27 -0400

    Added Number Wizard Files

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] TextMeshProUGUI triesText;
+
+    int guess;
+    int triesRemaining = 10;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+
+    // Change Guess
+    void takeGuess(){
+        guess = (min + max) / 2;
+        // GuessingText is on the GUI
+        guessText.text = guess.ToString();
+    }
+	
+    // Decrease number of tries
+    void tryToGuess(){
+        triesRemaining -= 1;
+        triesText.text = triesRemaining.ToString();
+    }
+
+    // Main Function 
+    void StartGame()
+    {
+        takeGuess();
+        tryToGuess();
+        max += 1;
+    }
+
+    public void OnPressHigher(){
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower(){
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        tryToGuess();
+        takeGuess();
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene(){
+
+        // Getting the currently active scene, find its buildIndex
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    
+    }
+
+    public void LoadStartScene()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
